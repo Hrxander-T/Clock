fbea3eb3d4c7bb312fb80109ffda6fececdebc3e
Hrxander <hrtahsinhossain@gmail.com>
2025-03-26 17:27:21 +0600

    Promise handeled

--- digital_timer.js
+++ digital_timer.js
@@ -1,3 +1,18 @@
+async function loadImage(src) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img); // Resolve the promise when the image loads
+    img.onerror = () => reject(new Error(`Failed to load image: ${src}`)); // Reject on error
+    img.src = src;
+  });
+}
+
+// Usage example
+async function main() {
+  
+
+let load= await loadImage('/img/white_vase.webp');
+  
 
 function time() {
   const now = new Date();
@@ -6,9 +21,13 @@ function time() {
   let seconds = now.getSeconds();
 
   let current_time =
-   `${hours>=10?hours:'0'+hours}:${minutes>=10?minutes:'0'+minutes}:${seconds>=10?seconds:'0'+seconds}`;
-  document.getElementById("clock").innerHTML =  current_time;
+    `${hours >= 10 ? hours : '0' + hours}:${minutes >= 10 ? minutes : '0' + minutes}:${seconds >= 10 ? seconds : '0' + seconds}`;
+  document.getElementById("clock").innerHTML = current_time;
 }
 
 time();
 setInterval(time, 1000);
+}
+main()
+
+
